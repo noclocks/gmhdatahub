12d8271237021f673918e14d37f62c6fb1462d6f
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-02-17 18:49:58 -0500

    feat: enhance xl report name function

--- R/utils_excel.R
+++ R/utils_excel.R
@@ -6,11 +6,13 @@ get_xl_report_file_name <- function(
 
   report_date_str <- format(as.Date(report_date), "%Y-%m-%d")
   report_file_ext <- ".xlsx"
-  abbrs <- c("GMH")
+  abbrs <- c("GMH", "AGC", "CBRE", "JHU", "AEW", "CRG")
+  replace <- c("Gmh" = "GMH", "Agc" = "AGC", "Cbre" = "CBRE", "Jhu" = "JHU", "Aew" = "AEW", "Crg" = "CRG")
 
   report_name <- snakecase::to_title_case(report_name) |>
     stringr::str_replace_all(" ", "_") |>
-    stringr::str_replace_all("Gmh", "GMH")
+    # replace all abbreviations with uppercase
+    stringr::str_replace_all(replace)
 
   paste0(
     report_date_str,
