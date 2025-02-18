0b819ad9d1f28db7160f2c8e32e2378478826516
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-02-18 10:21:31 -0500

    refactor: separate shiny utils from general utils and add roxygen prose

--- R/utils.R
+++ R/utils.R
@@ -1,78 +1,26 @@
-welcome_banner_ui <- function() {
-  bslib::layout_columns(
-    col_widths = c(12),
-    bslib::card(
-      class = "my-3",
-      bslib::card_header(
-        class = "bg-primary",
-        htmltools::tags$h2(
-          "Welcome to GMH Communities Data Hub",
-          class = "mb-0"
-        )
-      ),
-      bslib::card_body(
-        htmltools::tags$p(
-          class = "lead",
-          "Your centralized platform for student housing portfolio analytics and insights"
-        )
-      )
-    )
-  )
-}
 
-gmaps_properties_map_embed_iframe <- function(width = "100%", height = "500px") {
-  url <- "https://www.google.com/maps/d/embed?mid=19tP5Bf66khGcrNnqTBsk879W2fS-u7U&ehbc=2E312F"
+#  ------------------------------------------------------------------------
+#
+# Title : General Utilities
+#    By : Jimmy Briggs
+#  Date : 2025-02-18
+#
+#  ------------------------------------------------------------------------
 
-  htmltools::tags$iframe(
-    src = url,
-    width = width,
-    height = height
-  )
-}
+#' NULL Coalescing Operator
+#'
+#' @description
+#' This function is a wrapper for the NULL coalescing operator:
+#' If x if `NULL`, return y, else return x.
+#'
+#' @param x, y Values to compare.
+#'
+#' @keywords internal
+`%||%` <- rlang::`%||%`
 
-with_loader <- function(x) {
-  shinycustomloader::withLoader(x)
-}
-
-with_tooltip <- function(input, tooltip_text, placement = "right") {
-  current_label <- input$children[[1]]$children[[1]]
-
-  updated_label <- htmltools::tags$span(
-    current_label,
-    bslib::tooltip(
-      bsicons::bs_icon("info-circle", style = "cursor: help;"),
-      tooltip_text,
-      placement = placement
-    )
-  )
-
-  input$children[[1]]$children[[1]] <- updated_label
-
-  return(input)
-}
-
-icon_text <- function(icon, text, .function = shiny::icon) {
-  if (is.character(icon) && length(icon) == 1) {
-    icon <- .function(icon)
-  }
-
-  text <- paste0(" ", text)
-
-  htmltools::tags$span(
-    icon,
-    text
-  )
-}
-
-text_icon <- function(text, icon, .function = shiny::icon) {
-  icon <- .function(icon)
-  text <- paste0(text, " ")
-
-  htmltools::tags$span(
-    text,
-    icon
-  )
-}
+#' Inverted versions of in
+#' @noRd
+`%notin%` <- Negate(`%in%`)
 
 parse_request <- function(req) {
   http_method <- req$REQUEST_METHOD
@@ -174,20 +122,3 @@ get_amenity_id_by_name <- function(amenity_name) {
 
   return(amenity_id)
 }
-
-#' NULL Coalescing Operator
-#'
-#' @description
-#' This function is a wrapper for the NULL coalescing operator:
-#' If x if `NULL`, return y, else return x.
-#'
-#' @param x, y Values to compare.
-#'
-#' @keywords internal
-`%||%` <- rlang::`%||%`
-
-#' Inverted versions of in
-#' @noRd
-`%notin%` <- Negate(`%in%`)
-
-
