6842a26f74775dcb8bc056382405f4e759237468
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-03 14:02:13 -0500

    feat: add database survey.fee_structures table and seed SQL

---
+++ inst/database/schemas/survey/seed/seed.survey.fee_structures.sql
@@ -0,0 +1,3 @@
+INSERT INTO survey.fee_structures (property_id, property_name, fee_structure, created_by, updated_by)
+VALUES
+  (739085, '1047 Commonwealth Avenue', 'Both Fees Waived', '53b1207a-9066-49e4-9fcd-a6f439159759', '53b1207a-9066-49e4-9fcd-a6f439159759');
--- inst/database/schemas/survey/tables/survey.fee_structures.sql
+++ inst/database/schemas/survey/tables/survey.fee_structures.sql
@@ -4,6 +4,14 @@ CREATE TABLE survey.fee_structures (
   property_id INTEGER REFERENCES survey.properties(property_id),
   competitor_id INTEGER REFERENCES survey.competitors(competitor_id),
   property_name TEXT NOT NULL,
-  leasing_week_id INTEGER NOT NULL REFERENCES survey.leasing_weeks(leasing_week_id),
-  fee_structure TEXT NOT NULL CHECK (fee_structure IN ('Both Fees Waived', 'Both Fees Due', 'App Due - Admin Waived', 'Admin Due - App Waived'))
+  fee_structure TEXT NOT NULL DEFAULT 'Both Fees Waived' CHECK (fee_structure IN ('Both Fees Waived', 'Both Fees Due', 'App Due - Admin Waived', 'Admin Due - App Waived')),
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  created_by UUID REFERENCES survey.users(user_id),
+  updated_by UUID REFERENCES survey.users(user_id),
+  PRIMARY KEY (property_name, fee_structure),
+  CONSTRAINT chk_property_or_competitor CHECK (
+    (property_id IS NOT NULL AND competitor_id IS NULL) OR
+    (property_id IS NULL AND competitor_id IS NOT NULL)
+  )
 );
