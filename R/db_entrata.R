d6952c57995929ac5aa8d6f9f4d1944a72ff1ba9
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-21 15:41:50 -0400

    feat(db)!: add upsert function for pre-lease report by unit
    resolves #123
    
    Introduced a new function `db_upsert_entrata_pre_lease_report_by_unit` to handle the upsertion of pre-lease report data at the unit level. This enhances the existing functionality by allowing more granular updates based on individual units, improving data management and reporting accuracy.
    
    BREAKING CHANGE: The signature of `db_upsert_entrata_pre_lease_summary_by_property_unit` has been simplified, removing unnecessary parameters.

--- R/db_entrata.R
+++ R/db_entrata.R
@@ -76,6 +76,20 @@ db_upsert_entrata_pre_lease_report_by_property <- function(pool, data_lst = NULL
 
 }
 
+db_upsert_entrata_pre_lease_report_by_unit <- function(pool, data_lst) {
+
+  check_db_conn(pool)
+
+  summary_tbl <- purrr::pluck(data_lst, "summary")
+  details_tbl <- purrr::pluck(data_lst, "details")
+  execution_details_tbl <- purrr::pluck(data_lst, "execution_details")
+
+  db_upsert_entrata_pre_lease_summary_by_property_unit(pool, summary_tbl)
+  db_upsert_entrata_pre_lease_details_by_property(pool, details_tbl)
+  db_upsert_entrata_report_execution(pool, execution_details_tbl)
+
+}
+
 db_upsert_entrata_report_execution <- function(pool, execution_details) {
 
   check_db_conn(pool)
@@ -331,15 +345,7 @@ db_upsert_entrata_pre_lease_details_by_property <- function(pool, details_data) 
 
 
 
-db_upsert_entrata_pre_lease_summary_by_property_unit <- function(
-    pool,
-    summary_data,
-    report_req_json = NULL,
-    report_resp_json = NULL,
-    queue_req_json = NULL,
-    queue_resp_json = NULL,
-    report_params_json = NULL
-) {
+db_upsert_entrata_pre_lease_summary_by_property_unit <- function(pool, summary_data) {
 
   check_db_conn(pool)
 
@@ -483,12 +489,6 @@ db_upsert_entrata_pre_lease_by_property <- function(pool, resp_lst) {
 
         summary_data <- purrr::pluck(resp_lst, "summary_data")
         details_data <- purrr::pluck(resp_lst, "details_data")
-        # report_req_json <- purrr::pluck(resp_lst, "report_req_json")
-        # report_resp_json <- purrr::pluck(resp_lst, "report_resp_json")
-        # queue_req_json <- purrr::pluck(resp_lst, "queue_req_json")
-        # queue_resp_json <- purrr::pluck(resp_lst, "queue_resp_json")
-        # report_params_json <- purrr::pluck(resp_lst, "report_params_json")
-        # queue_id <- purrr::pluck(resp_lst, "queue_id")
 
         dbx::dbxUpsert(
           conn,
@@ -517,82 +517,6 @@ db_upsert_entrata_pre_lease_by_property <- function(pool, resp_lst) {
             "Successfully upserted {.field details_data} to {.field entrata.pre_lease_details_by_property}."
           )
         )
-
-        # if (
-        #   !is.null(report_req_json) &&
-        #   !is.null(report_resp_json) &&
-        #   !is.null(queue_req_json) &&
-        #   !is.null(queue_resp_json) &&
-        #   !is.null(report_params_json) &&
-        #   !is.null(queue_id)
-        # ) {
-        #
-        #   queue_start <- jsonlite::fromJSON(queue_resp_json) |>
-        #     purrr::pluck("response", "result", "queueStartedOn") |>
-        #     lubridate::mdy_hms()
-        #
-        #   queue_end <- jsonlite::fromJSON(queue_resp_json) |>
-        #     purrr::pluck("response", "result", "queueCompletedOn") |>
-        #     lubridate::mdy_hms()
-        #
-        #   queue_duration <- as.integer(difftime(queue_end, queue_start, units = "secs"))
-        #
-        #   request_id <- jsonlite::fromJSON(report_req_json) |>
-        #     purrr::pluck("request_id")
-        #
-        #   # minify json
-        #   report_req_json <- jsonlite::toJSON(jsonlite::fromJSON(report_req_json), auto_unbox = TRUE)
-        #   report_resp_json <- jsonlite::toJSON(jsonlite::fromJSON(report_resp_json), auto_unbox = TRUE)
-        #   queue_req_json <- jsonlite::toJSON(jsonlite::fromJSON(queue_req_json), auto_unbox = TRUE)
-        #   queue_resp_json <- jsonlite::toJSON(jsonlite::fromJSON(queue_resp_json), auto_unbox = TRUE)
-        #   report_params_json <- jsonlite::toJSON(jsonlite::fromJSON(report_params_json), auto_unbox = TRUE)
-        #
-        #   logs_data_report <- tibble::tibble(
-        #     log_date = Sys.Date(),
-        #     entrata_endpoint = "/reports",
-        #     entrata_operation = "getReportData",
-        #     entrata_request_id = request_id,
-        #     entrata_request_start = as.POSIXct(NA),
-        #     entrata_request_end = as.POSIXct(NA),
-        #     entrata_request_duration = as.integer(NA),
-        #     entrata_request_json = as.character(report_req_json),
-        #     entrata_response_json = as.character(report_resp_json),
-        #     entrata_report_params_json = as.character(report_params_json),
-        #     entrata_report_name = "pre_lease",
-        #     entrata_report_queue_id = queue_id,
-        #     log_comments = "Pre-Lease Report by Unit Type Report Request/Response"
-        #   )
-        #
-        #   logs_data_queue <- tibble::tibble(
-        #     log_date = Sys.Date(),
-        #     entrata_endpoint = "/queue",
-        #     entrata_operation = "getResponse",
-        #     entrata_request_id = request_id,
-        #     entrata_request_start = queue_start,
-        #     entrata_request_end = queue_end,
-        #     entrata_request_duration = queue_duration,
-        #     entrata_request_json = as.character(queue_req_json),
-        #     entrata_response_json = as.character(queue_resp_json),
-        #     entrata_report_params_json = as.character(report_params_json),
-        #     entrata_report_name = "pre_lease",
-        #     entrata_report_queue_id = queue_id,
-        #     log_comments = "Pre-Lease Report by Unit Type Queue Request/Response"
-        #   )
-        #
-        #   logs_data <- dplyr::bind_rows(logs_data_report, logs_data_queue)
-        #
-        #   dbx::dbxInsert(
-        #     conn,
-        #     table = DBI::SQL("entrata.api_logs"),
-        #     records = logs_data
-        #   )
-        #
-        #   cli::cli_alert_success(
-        #     c(
-        #       "Successfully inserted API request/response logs into {.field entrata.api_logs}."
-        #     )
-        #   )
-        # }
       }
     )
   }, error = function(e) {
